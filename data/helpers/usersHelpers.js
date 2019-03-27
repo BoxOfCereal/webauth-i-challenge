9ae653707966a11b31d0f816877b19ec7391a19c
Noah <noah.stoeckel@gmail.com>
2019-03-26 23:38:39 -0400

    Adds Users Helpers

---
+++ data/helpers/usersHelpers.js
@@ -0,0 +1,24 @@
+const db = require("../dbconfig");
+
+module.exports = {
+  getUserByName,
+  addUser,
+  getAllUsers
+};
+
+function getUserByName(username) {
+  return db("users")
+    .where({ username })
+    .first();
+}
+
+function getAllUsers() {
+  return db("users");
+}
+
+function addUser(user) {
+  return db
+    .insert(user)
+    .into("users")
+    .first();
+}
