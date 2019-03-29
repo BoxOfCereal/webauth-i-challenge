8ca173aa392fd0ed319418f5da94b50d4846481e
Noah <noah.stoeckel@gmail.com>
2019-03-28 23:43:22 -0400

    Fixes Import For Knex Session Store

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const session = require("express-session");
-const KnexSessionStore = require("connect-session-knex");
+const KnexSessionStore = require("connect-session-knex")(session);
 
 const server = express();
 
