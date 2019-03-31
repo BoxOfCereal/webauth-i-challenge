d0a0f3b67b914bf26b28b68aa41f4a8e2658a7f0
Noah <noah.stoeckel@gmail.com>
2019-03-31 12:27:15 -0400

    Configure Cors To Use Credentials

--- server.js
+++ server.js
@@ -30,7 +30,19 @@ server.use(
 );
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+
+//CORS OPTIONS
+const whitelist = ["http://example1.com", "http://example2.com"];
+const corsOptionsDelegate = function(req, callback) {
+  let corsOptions;
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+server.use(cors(corsOptionsDelegate));
 
 //global middleware
 const { protected } = require("./middleware/auth");
